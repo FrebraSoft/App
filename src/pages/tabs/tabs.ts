98038eecb3e7556034e467f79848fc41236a9055
velrino <denismagalhaesvelrino@gmail.com>
2018-06-10 01:51:31 -0300

    Tabs default pages

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { HomePage } from '../home/home';
 import { Platform } from 'ionic-angular';
 import { ProfilePage } from '../profile/profile';
+import { SimulatorPage } from '../simulator/simulator';
 
 @Component({
   selector: 'page-tabs',
@@ -11,7 +12,7 @@ import { ProfilePage } from '../profile/profile';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = HomePage;
+  tab2Root = SimulatorPage;
   tab3Root = ProfilePage;
 
   constructor(public platform : Platform) {}
